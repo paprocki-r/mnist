6aa694f3408201798eec4c5cc1ae4f4a734d7d7f
Rafal Paprocki <rafal.paprocki@laerdal.com>
2024-07-08 15:37:59 +0200

    bugs  and solutions in comments

--- get_dataset.py
+++ get_dataset.py
@@ -1,11 +1,11 @@
 from sklearn.datasets import fetch_openml
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_spl #train_test_spl -> train_test_split
 from sklearn.utils import check_random_state
 
 
 def get_dataset(train_samples:int)->list:        
     # Load data from https://www.openml.org/d/554
-    X, y = fetch_openml("mnist_784", version=1, return_X_y=True, as_frame=False)
+    X, y = fetch_openml("mnist_78", version=1, return_X_y=False, as_frame=False) #1. Data loading bug: "mnist_78"->"mnist_784",  return_X_y=False -> return_X_y=True
 
     random_state = check_random_state(0)
     permutation = random_state.permutation(X.shape[0])
@@ -13,6 +13,6 @@ def get_dataset(train_samples:int)->list:
     y = y[permutation]
     X = X.reshape((X.shape[0], -1))
 
-    return train_test_split(
+    return train_test_spl(
         X, y, train_size=train_samples, test_size=10000
     )
